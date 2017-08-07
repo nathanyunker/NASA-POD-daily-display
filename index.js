9fb9a188f692bd4a391a4bf2cf56bdbeba3e80ff
Nathan Yunker <nathanyunker@catalystitservices.com>
2017-08-07 11:53:53 -0700

    retrieving POD with APOD API, downloading to image folder, and setting as background with wallpaper

---
+++ index.js
@@ -0,0 +1,62 @@
+const apod = require("apod");
+const config = require('./config');
+const download = require('image-downloader')
+const wallpaper = require('wallpaper');
+ 
+// Create a config.js file in the root directory with <apiKey> and <imgDest> attributes. Example below:
+// let config = {};
+
+// config = {
+//   apiKey: 'Enter Your NASA APOD API key here',
+//   imageDest: 'Enter the path of desired file location, including desired file name. Example: 'C:/path/to/dest/photo.jpg''
+// };
+
+// module.exports = config;
+
+
+
+apod.apiKey = config.apiKey;
+
+function callback(err, data) {
+  if (err) {
+    console.log("ERROR recieving NASA Picture of the day: ", err);
+  } else {
+    console.log('Recieved NASA Picture of the Day...');
+    saveImage(data.hdurl || data.url)
+  }
+}
+
+function saveImage(imageUrl) {
+  console.log("Attempting to save picutre to " + config.imageDest)
+
+  const options = {
+    url: imageUrl,
+    dest: config.imageDest 
+  }
+   
+  download.image(options)
+    .then(({ filename, image }) => {
+      console.log('File saved successfully')
+      setWallpaper();
+    }).catch((err) => {
+      throw err
+    })
+}
+ 
+function setWallpaper() {
+  wallpaper.set(config.imageDest)
+    .then(() => {
+      console.log('WALLPAPER SET!');
+  });
+}
+
+// get today's APOD:
+//apod(callback);
+
+// APOD for a specific date:
+//apod("December 31, 1999", callback);
+//or
+//apod(1999, 11, 31, callback);
+
+// random APOD: 
+apod.random(callback);
